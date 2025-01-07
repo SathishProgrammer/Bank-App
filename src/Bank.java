1331e670c5c487a91be9302ad8dd8707fb8d7620
SathishProgrammer <sathishkumar30320@gmail.com>
2025-01-07 22:46:36 +0530

    first commit

---
+++ src/Bank.java
@@ -0,0 +1,125 @@
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Bank {
+
+  private List<Account>customers;
+  Scanner scan = new Scanner(System.in);
+
+  public Bank() {
+    this.customers = new ArrayList<>();
+  }
+
+  public void addCustomer(Account customer){
+    customers.add(customer);
+  }
+
+  public void removeCustomer(){
+	  Account account = getAccount();
+	  try {
+		  if(account == null) {
+			  throw new CustomerNotFoundException("Account not found");
+		  }
+		  customers.remove(account);
+	  }
+	  catch(CustomerNotFoundException e) {
+		  System.out.println(e.getMessage());
+		  removeCustomer();
+	  }
+  }
+  
+  public Account getAccount() {
+	  
+	  System.out.print("Enter Account Number: ");
+      String accNo = scan.nextLine();
+	  for (Account account : customers) {
+	        if (account.getAccountNumber().equals(accNo)){
+	          return account;
+	        }
+	  }
+	  return null;
+  }
+  
+  public void cashDeposit() {
+	  Account account = getAccount();
+	  try {
+		  if(account == null) {
+			  throw new CustomerNotFoundException("Incorrect Account Number");
+		  }
+		  
+		  System.out.print("Enter the amount: ");
+		  int amount = scan.nextInt();
+		  scan.nextLine(); // to consume leftover new line
+		  
+		  if(amount > 100) {
+			  System.out.println(account.deposit(amount)+"\n");
+		  }
+		  else {
+			  System.out.println("Amount should be atleast 100");			  
+		  }
+	  }
+	  catch (CustomerNotFoundException e) {
+		  System.out.println(e.getMessage());
+		  cashDeposit();
+	  }
+  }
+  
+  public void cashWithdraw(){
+	  Account account = getAccount();
+	  try {
+    	if(account == null) {
+    		throw new CustomerNotFoundException("Incorrect Account Number");
+    	}
+    	System.out.print("Enter the amount: ");
+    	int amount = scan.nextInt();
+    	scan.nextLine(); // to consume leftover new line
+    	
+	    if (amount > account.getInitialBalance()){
+	      System.out.println("Insuffiecient Balance"+"\n");
+	    }
+	    else{
+	      System.out.println("Balance: " + account.withdraw(amount)+"\n");
+	    }
+    }
+    catch(CustomerNotFoundException e){
+    	System.out.println(e.getMessage());
+    	cashWithdraw();
+    }  
+  }
+
+  public void getBalance(){
+	  Account account = getAccount();
+	  try {
+		  if(account == null) {
+			  throw new CustomerNotFoundException("Incorrect account number");
+		  }
+		  System.out.println(account.getInitialBalance()+"\n");
+	  }
+	  catch(CustomerNotFoundException e) {
+		  System.out.println(e.getMessage());
+		  getBalance();
+	  }
+  }
+
+  public void findCustomer(){
+	  Account account = getAccount();
+    try {
+    	if(account == null) {
+    		throw new CustomerNotFoundException("Customer not found.");    		
+    	}
+    	System.out.println(account);
+    }
+    catch (CustomerNotFoundException e) {
+		System.out.println(e.getMessage());
+		findCustomer();
+	}
+  
+  }
+
+  public void displayAllCustomers(){
+    for (Account customer : customers) {
+      System.out.println(customer);
+    }
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,59 @@
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Bank bank = new Bank();
+    Account account = null;
+    Scanner scan = new Scanner(System.in);
+    while(true){
+      System.out.println("Welcome to Indian Bank");
+      System.out.println(" ");
+      System.out.println("Select the option to perform: ");
+      System.out.println(" ");
+      System.out.println("1: Open Account" + "\n" +"2: View Account" + "\n" +"3: Close Account" + "\n" +
+              "4: Deposit Amount" + "\n" + "5: Withdraw Amount" + "\n" + "6: Check Balance" + "\n" + "7: Exit"+ "\n");
+
+      int choice = scan.nextInt();
+      scan.nextLine();
+      switch (choice) {
+        case 1:
+          account = new Account();
+          account.openAccount();
+          bank.addCustomer(account);
+          break;
+
+        case 2:
+          bank.findCustomer();
+          break;
+          
+        case 3:
+          bank.removeCustomer();
+          break;
+
+        case 4:
+              bank.cashDeposit();;
+              break;
+
+        case 5:
+              bank.cashWithdraw();
+              break;
+          
+        case 6:
+          bank.getBalance();
+          break;
+
+        default:
+          if(choice != 7){
+            System.out.println("Enter a valid input: ");
+      }
+          break;
+    }
+    if (choice==7){
+      System.out.println("Thank you for banking with us!!");
+      break;
+    }
+  }
+    scan.close();
+    }
+}
